5d1050bd9eed6380a46a4336bd52fd4a5d1bbf4d
Matthew Haines-Young <matthew@humanmade.co.uk>
2011-05-16 16:34:44 +0100

    - An example WPThumb plugin that would allow you to rotate images.

--- example.rotate.php
+++ example.rotate.php
@@ -8,9 +8,8 @@ Version: 0.1
 Author URI: http://www.humanmade.co.uk/
 */
 
-
-
-
+// This makes use of filters in WPThumb that allow you to manipulate the image object using any of the phpThumb functions.
+// In this case - I have set a custom argument when calling the attachment eg wp_get_attachment_image( $id ,array( 'thumbnail', 'custom' => 'rotator' ));
 
 add_filter( 'wpthumb_filename_custom', 'rotator_filename', 10, 2 );
 add_filter( 'wpthumb_image_filter', 'rotator_action', 10, 2 );
@@ -25,6 +24,7 @@ function rotator_action( $thumb, $args ) {
 	
 }
 
+//Filename must also be different from standard. 
 function rotator_filename( $custom, $args ){	
 	$custom = $args['custom'];
 	return $custom;
