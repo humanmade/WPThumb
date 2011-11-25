ca3cef64487db15fd42b067fcf01a8bb54635e46
Joe Hoyle <joehoyle@gmail.com>
2011-11-25 13:53:25 +0000

    Added license

--- wpthumb.php
+++ wpthumb.php
@@ -8,6 +8,23 @@ Version: 0.5
 Author URI: http://www.hmn.md/
 */
 
+/*  Copyright 2011 Human Made Limited  (email : hello@humanmade.co.uk)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
 class WP_Thumb {
 	
 	private $args;
