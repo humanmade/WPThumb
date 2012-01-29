90c1f12a1a8e404ba86237649d926055b3793b14
Tom Willmot <tom@humanmade.co.uk>
2012-01-29 15:13:58 +0100

    code formatting

--- watermark-actions.php
+++ watermark-actions.php
@@ -1,7 +1,6 @@
-<?php
-include_once( '../../../../wp-load.php' );
-if( $_GET['action'] === 'wpthumb_wm_watermark_preview_image' ) {
+<?php include_once( '../../../../wp-load.php' );
+
+if ( $_GET['action'] === 'wpthumb_wm_watermark_preview_image' )
 	echo wpthumb_wm_watermark_preview_image( $_GET['position'], $_GET['padding'], $_GET['image_id'], $_GET['mask'] );
-}
+
 exit;
-?>
