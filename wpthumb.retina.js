2cd074c692abd83d59f685adc917ee84a53511b0
Tom Willmot <tom@humanmade.co.uk>
2012-05-28 20:13:39 +0100

    Remove console.log's, namespace functions and fix logic in classname check.

--- wpthumb.retina.js
+++ wpthumb.retina.js
@@ -1,8 +1,10 @@
-// Add Event Listener wrapper function
-var addEvent=function(){return document.addEventListener?function(a,c,d){if(a&&a.nodeName||a===window)a.addEventListener(c,d,!1);else if(a&&a.length)for(var b=0;b<a.length;b++)addEvent(a[b],c,d)}:function(a,c,d){if(a&&a.nodeName||a===window)a.attachEvent("on"+c,function(){return d.call(a,window.event)});else if(a&&a.length)for(var b=0;b<a.length;b++)addEvent(a[b],c,d)}}();
+// Add Event Listener utility function
+var wpthumbAddEvent=function(){return document.addEventListener?function(a,c,d){if(a&&a.nodeName||a===window)a.addEventListener(c,d,!1);else if(a&&a.length)for(var b=0;b<a.length;b++)wpthumbAddEvent(a[b],c,d)}:function(a,c,d){if(a&&a.nodeName||a===window)a.attachEvent("on"+c,function(){return d.call(a,window.event)});else if(a&&a.length)for(var b=0;b<a.length;b++)wpthumbAddEvent(a[b],c,d)}}();
 
-
-function getDevicePixelRatio() {
+/**
+ * Wrap window.devicePixelRatio
+ */
+function wpthumbGetDevicePixelRatio() {
 
 	if ( typeof( window.devicePixelRatio ) === undefined )
 		return 1;
@@ -11,43 +13,38 @@ function getDevicePixelRatio() {
 
 }
 
-
-var responsiveEnhance = function( img, monitor ) {
+var wpthumbResponsiveEnhance = function( img ) {
 		
-	// Can pass an array of images. 
-	// If so, loop through calling this function again for each.
-	if (img.length) {
+	// If passed an array of images loop through calling this function again for each.
+	if ( img.length ) {
 	
-		for (var i=0, len=img.length; i<len; i++) {
-            responsiveEnhance(img[i], img.clientWidth, monitor);
+		for ( var i = 0, len = img.length; i < len; i++ ) {
+            wpthumbResponsiveEnhance( img[i] );
         }
 
 	} else {
 	
-		// Don't do this if we have done it already ( has class large )
-		if ( img.getAttribute( 'data-retina-src') !== undefined || ( ( ' ' + img.className + ' ' ).replace( /[\n\t]/g, ' ' ).indexOf(' image-retina ') !== -1 ) ) {
+		// Only run once per image
+		if ( img.getAttribute( 'data-retina-src') !== undefined && ( ( ' ' + img.className + ' ' ).replace( /[\n\t]/g, ' ' ).indexOf( ' image-retina ' ) === -1 ) ) {
 	
 			// Create a new image from
 			// Once loaded, replace the original image src.
 			// Add class image-retina.
 			var fullimg = new Image();
+
 			fullimg.src = img.getAttribute( 'data-retina-src' );    	
-  			addEvent( fullimg, 'load', function(e) {
-  			
-  				console.log( this.src );
+
+  			wpthumbAddEvent( fullimg, 'load', function( e ) {
   				img.src = this.src;
 				img.className += ' image-retina';
-			});
-  		
+			} );
+ 		
   		}
-  		
+
 	}
 		
 };
 
-console.log( getDevicePixelRatio() );
-
-// Call the function if we are on a high resoloution screen.
-if( 1.5 <= getDevicePixelRatio() ) {
-	responsiveEnhance( document.getElementsByTagName('img') );
-}
+// Call the function if we are on a high resoloution screen
+if ( 1.5 < wpthumbGetDevicePixelRatio() )
+	wpthumbResponsiveEnhance( document.getElementsByTagName( 'img' ) );
