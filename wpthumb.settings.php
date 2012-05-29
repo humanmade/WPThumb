f28ab94b25bcf6740bf3c30d6fc97bb39b946fbf
Tom Willmot <tom@humanmade.co.uk>
2012-05-29 08:26:49 +0100

    Duh! don't redeclare functions

--- wpthumb.settings.php
+++ wpthumb.settings.php
@@ -3,13 +3,13 @@
 /**
  * Add the retina setting field to the media settings page
  */
-function wpthumb_retina_setting_field() {
+function wpthumb_retina_setting() {
 	
 	register_setting( 'media', 'wpthumb_retina' );
 	add_settings_field( 'wpthumb_retina', 'Add Retina support to thumbnails', 'wpthumb_retina_setting_field', 'media' );
 
 }
-add_action( 'admin_init', 'wpthumb_retina_setting_field' );
+add_action( 'admin_init', 'wpthumb_retina_setting' );
 
 /**
  * Output the retina setting field
