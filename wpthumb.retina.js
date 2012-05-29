616365742307a52d6598e55c57d27fa7d56c553d
Matthew Haines-Young <matthew@humanmade.co.uk>
2012-05-29 16:56:04 +0100

    fix up the js a bit nicer

--- wpthumb.retina.js
+++ wpthumb.retina.js
@@ -1,6 +1,7 @@
 // Add Event Listener utility function
 var wpthumbAddEvent=function(){return document.addEventListener?function(a,c,d){if(a&&a.nodeName||a===window)a.addEventListener(c,d,!1);else if(a&&a.length)for(var b=0;b<a.length;b++)wpthumbAddEvent(a[b],c,d)}:function(a,c,d){if(a&&a.nodeName||a===window)a.attachEvent("on"+c,function(){return d.call(a,window.event)});else if(a&&a.length)for(var b=0;b<a.length;b++)wpthumbAddEvent(a[b],c,d)}}();
 
+
 /**
  * Wrap window.devicePixelRatio
  */
@@ -13,38 +14,51 @@ function wpthumbGetDevicePixelRatio() {
 
 }
 
-var wpthumbResponsiveEnhance = function( img ) {
-		
+
+/** 
+ *	Enhance images if required.
+ *
+ *	If we are on a high res device and the image has a high res src available, load that in.
+ */
+var wpthumbRetinaEnhanceAction = function( img ) {
+
 	// If passed an array of images loop through calling this function again for each.
 	if ( img.length ) {
 	
 		for ( var i = 0, len = img.length; i < len; i++ ) {
-            wpthumbResponsiveEnhance( img[i] );
+            wpthumbRetinaEnhanceAction( img[i] );
         }
-
+ 
 	} else {
 	
-		// Only run once per image
-		if ( img.getAttribute( 'data-retina-src') !== undefined && ( ( ' ' + img.className + ' ' ).replace( /[\n\t]/g, ' ' ).indexOf( ' image-retina ' ) === -1 ) ) {
+		// Only run once per image 		 		
+ 		if ( img.getAttribute( 'data-retina-src') !== undefined && ( ( ' ' + img.className + ' ' ).replace( /[\n\t]/g, ' ' ).indexOf( ' image-retina ' ) === -1 ) ) {
 	
 			// Create a new image from
 			// Once loaded, replace the original image src.
 			// Add class image-retina.
-			var fullimg = new Image();
-
+			
+			var fullimg = new Image();	
 			fullimg.src = img.getAttribute( 'data-retina-src' );    	
-
+ 
   			wpthumbAddEvent( fullimg, 'load', function( e ) {
   				img.src = this.src;
+	  			img.src = this.src;
 				img.className += ' image-retina';
+				
 			} );
- 		
-  		}
-
+			
+		}
+	
 	}
-		
-};
 
-// Call the function if we are on a high resoloution screen
-if ( 1.5 < wpthumbGetDevicePixelRatio() )
-	wpthumbResponsiveEnhance( document.getElementsByTagName( 'img' ) );
+}	
+
+var wpthumbRetinaEnhance = function() {
+	
+	if( 1 <= wpthumbGetDevicePixelRatio() )
+		wpthumbRetinaEnhanceAction( document.getElementsByTagName( 'img' ) );
+	
+}
+
+window.onload = wpthumbRetinaEnhance();
