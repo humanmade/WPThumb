4a783b839980a07c7426e7bcc61eb84ac07e5da9
Matth_eu <matthew@matth.eu>
2013-04-04 12:29:32 +0100

    handle images in other wp_content_dir directories.

--- wpthumb.php
+++ wpthumb.php
@@ -302,6 +302,10 @@ class WP_Thumb {
 			$subdir = dirname( str_replace( $upload_dir['basedir'], '', $this->getFilePath() ) );
 			$new_dir = $upload_dir['basedir'] . '/cache' . $subdir . '/' . $filename_nice;
 
+		elseif ( strpos( $this->getFilePath(), WP_CONTENT_DIR ) === 0 ) :
+
+			$subdir = dirname( str_replace( WP_CONTENT_DIR, '', $this->getFilePath() ) );
+			$new_dir = $upload_dir['basedir'] . '/cache' . $subdir . '/' . $filename_nice;
 
 		elseif ( strpos( $this->getFilePath(), self::get_home_path() ) === 0 ) :
 			$new_dir = $upload_dir['basedir'] . '/cache/local';
