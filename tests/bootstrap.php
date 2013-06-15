350ae6811ef5a15d14116f8069080dd97ed52790
Joe Hoyle <joehoyle@gmail.com>
2013-06-15 10:50:18 +0100

    New bootstrap for new tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,21 +1,13 @@
 <?php
-/**
- * Bootstrap the testing environment
- * Uses wordpress tests (http://github.com/nb/wordpress-tests/) which uses PHPUnit
- * @package wordpress-plugin-tests
- *
- * Usage: change the below array to any plugin(s) you want activated during the tests
- *        value should be the path to the plugin relative to /wp-content/
- *
- * Note: Do note change the name of this file. PHPUnit will automatically fire this file when run.
- *
- */
 
-$GLOBALS['wp_tests_options'] = array(
-	'active_plugins' => array( 'WPThumb/wpthumb.php' ),
-);
+require_once getenv( 'WP_TESTS_DIR' ) . '/includes/functions.php';
 
-require dirname( __FILE__ ) . '/lib/bootstrap.php';
+function _manually_load_plugin() {
+	require dirname( __FILE__ ) . '/../wpthumb.php';
+}
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+require getenv( 'WP_TESTS_DIR' ) . '/includes/bootstrap.php';
 
 class WP_Thumb_UnitTestCase extends WP_UnitTestCase {
 
