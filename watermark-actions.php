bde25083aec92798d48c5d82c731f9f856a50fbf
Joe Hoyle <joehoyle@gmail.com>
2013-07-17 11:44:56 +0100

    Fixed inlcuding preview file if wp is in subdir

--- watermark-actions.php
+++ watermark-actions.php
@@ -1,4 +1,9 @@
-<?php include_once( '../../../../wp-load.php' );
+<?php 
+
+if ( file_exists( '../../../../wp-load.php' ) )
+	include_once( '../../../../wp-load.php' );
+else
+	include_once( '../../../wordpress/wp-load.php' );
 
 if ( $_GET['action'] === 'wpthumb_wm_watermark_preview_image' )
 	echo wpthumb_wm_watermark_preview_image( $_GET['position'], $_GET['padding'], $_GET['image_id'], $_GET['mask'] );
