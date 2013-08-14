c1b239a4d43e8dbd9c82fe92e0747ccf76000ab8
Joe Hoyle <joehoyle@gmail.com>
2013-08-14 19:57:18 +0100

    added ability to set attributes and also renamed to wpthumb

--- wpthumb.shortcodes.php
+++ wpthumb.shortcodes.php
@@ -1,23 +1,33 @@
 <?php
 
 /**
- * The [img] shortcode is used for resizing image URLs / attachments from within the content
+ * The [wpthumb] shortcode is used for resizing image URLs / attachments from within the content
  * 
- * The img shortcode supports all the WP Thumb arguments, for example:
+ * The wpthumb shortcode supports all the WP Thumb arguments, for example:
  * 
- * [img 4567 width=400 height=200]
+ * [wpthumb 4567 width=400 height=200]
  */
 
-add_shortcode( 'img', 'wpthumb_img_shortcode' );
+add_shortcode( 'wpthumb', 'wpthumb_img_shortcode' );
 
 function wpthumb_img_shortcode( $args ) {
+
+	$args_attrs = array( 'class', 'alt' );
+	$attrs = array();
+
+	foreach ( $args_attrs as $att ) {
+		if ( isset( $args[$att] ) ) {
+			$attrs[$att] = $args[$att];
+			unset( $args[$att] );
+		}
+	}
 
 	if ( is_numeric( $args[0] ) ) {
 
 		$attachment_id = $args[0];
 		unset( $args[0] );
 
-		return wp_get_attachment_image( $attachment_id, $args );
+		return wp_get_attachment_image( $attachment_id, $args, false, $attrs );
 
 	} else if ( ! empty( $args ) ) {
 
@@ -28,6 +38,12 @@ function wpthumb_img_shortcode( $args ) {
 
 		list( $width, $height ) = getimagesize( $image );
 
-		return '<img src="' . $image . '" width="' . $width . '" height="' . $height . '" />';
+		$attr = '';
+
+		foreach ( $attrs as $a => $value ) {
+			$attr .= ' ' . $a . '="' . esc_attr( $value ) . '"';
+		}
+
+		return '<img src="' . $image . '" width="' . $width . '" height="' . $height . '"' . $attr . ' />';
 	}
 }
