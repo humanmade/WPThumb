594b6760ae371556d7c3f69f4149836a02e69400
Joe Hoyle <joehoyle@gmail.com>
2013-10-18 16:49:50 +0100

    Tests for izes should make sure we _don't_ have the cache dir in them

--- tests/imageSizesTest.php
+++ tests/imageSizesTest.php
@@ -7,11 +7,15 @@ class WPThumbImageSizesTestCase extends WP_Thumb_UnitTestCase {
 
 	function setUp() {
 
+		add_filter( 'wp_image_editors', array( $this, 'set_wp_image_editor') );
+
 		$this->file = tempnam( get_temp_dir(), '' ) . '.png';
 
 		copy( dirname( __FILE__ ) . '/images/transparent.png', $this->file );
 
 		$this->attachment = wp_insert_attachment( array( 'post_title' => 'test attachment', 'post_content' => 'test content', 'post_type' => 'attachment', 'post_status' => 'publish', 'post_mime_type' => 'image/png' ), $this->file );
+
+		wp_update_attachment_metadata( $this->attachment, wp_generate_attachment_metadata( $this->attachment, $this->file ) );
 
 		update_option( 'thumbnail_size_w', 100 );
 		update_option( 'thumbnail_size_h', 100 );
@@ -29,6 +33,9 @@ class WPThumbImageSizesTestCase extends WP_Thumb_UnitTestCase {
 		wp_delete_post( $this->attachment, true );
 	}
 
+	function set_wp_image_editor() {
+		return array( 'WP_Thumb_Image_Editor_GD' );
+	}
 	/**
 	 * @group testThumbnailResize
 	 */
@@ -39,9 +46,12 @@ class WPThumbImageSizesTestCase extends WP_Thumb_UnitTestCase {
 		$this->assertEquals( 100, $image[1] );
 		$this->assertEquals( 100, $image[2] );
 
-		$this->assertTrue( (bool) strpos( $image[0], '/cache/' ) );
+		$this->assertFalse( (bool) strpos( $image[0], '/cache/' ) );
 	}
 
+	/**
+	 * @group testMediumResize
+	 */
 	function testMediumResize() {
 
 		$image = wp_get_attachment_image_src( $this->attachment, 'medium' );
@@ -49,7 +59,7 @@ class WPThumbImageSizesTestCase extends WP_Thumb_UnitTestCase {
 		$this->assertEquals( 200, $image[1] );
 		$this->assertEquals( 161, $image[2] );
 
-		$this->assertTrue( (bool) strpos( $image[0], '/cache/' ) );
+		$this->assertFalse( (bool) strpos( $image[0], '/cache/' ) );
 
 	}
 
@@ -60,7 +70,7 @@ class WPThumbImageSizesTestCase extends WP_Thumb_UnitTestCase {
 		$this->assertEquals( 300, $image[1] );
 		$this->assertEquals( 242, $image[2] );
 
-		$this->assertTrue( (bool) strpos( $image[0], '/cache/' ) );
+		$this->assertFalse( (bool) strpos( $image[0], '/cache/' ) );
 
 	}
 
@@ -72,9 +82,9 @@ class WPThumbImageSizesTestCase extends WP_Thumb_UnitTestCase {
 		$image = wp_get_attachment_image_src( $this->attachment, 'testSize' );
 
 		$this->assertEquals( 400, $image[1] );
-		$this->assertEquals( 400, $image[2] );
+		$this->assertEquals( 323, $image[2] );
 
-		$this->assertTrue( (bool) strpos( $image[0], '/cache/' ) );
+		$this->assertFalse( (bool) strpos( $image[0], '/cache/' ) );
 	}
 
 	function testResizedByArray() {
