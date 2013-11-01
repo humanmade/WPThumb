1864d843ce82063919ffd9db3275858e4162e9f0
Matth_eu <matthew@matth.eu>
2013-11-01 19:46:41 +0000

    unit test for an image enlarged in both dimensions

--- tests/backgroundFillTest.php
+++ tests/backgroundFillTest.php
@@ -27,4 +27,38 @@ class WPThumbBackgroundFillTestCase extends WP_Thumb_UnitTestCase {
 		
 		$this->assertImageRGBAtPoint( $file, array(1,1), array(255,255,255) );
 	}
+
+
+	/**
+	 * 100x100 black image.
+	 * Auto fill to 200x200 with red background
+	 * Expect red at mid point of each side.
+	 */
+	function testBackgroundFillRedForBlackImg() {
+		
+		$path = dirname( __FILE__ ) . '/images/black.png';
+		list( $width, $height ) = getimagesize( $path );
+		
+		$this->assertNotNull( $width );
+		$this->assertNotNull( $height );
+				
+		$image = new WP_Thumb( $path, "width=200&height=200&cache=0&return=path&background_fill=255000000" );
+		
+		$file = $image->returnImage();
+		
+		$this->assertContains( '/cache/', $file );
+		$this->assertContains( WP_CONTENT_DIR, $file );
+		
+		list( $new_width, $new_height ) = getimagesize( $file );
+		
+		$this->assertEquals( $new_width, 200, 'Width is not expected' );
+		$this->assertEquals( $new_height, 200, 'Height is not expcted' );
+
+		$this->assertImageRGBAtPoint( $file, array(0,100), array(255,000,000), 'Left middle is wrong' );
+		$this->assertImageRGBAtPoint( $file, array(100,0), array(255,000,000), 'Middle top is wrong' );
+		$this->assertImageRGBAtPoint( $file, array(199,100), array(255,000,000), 'Bottom center is wrong' );
+		$this->assertImageRGBAtPoint( $file, array(100,199), array(255,000,000), 'Middle bottom is wrong' );
+
+	}
+
 }
